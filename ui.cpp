1866d1403d2ae1b56248a0776b9691a332606cf9
Daniel Harvey <the.real.daniel.harvey@gmail.com>
2024-12-15 17:56:28 +0000

    full mix parameter scaling for highpass.

--- ui.cpp
+++ ui.cpp
@@ -3,14 +3,14 @@
 
 /** CONSTANTS */
 
-static const float a1 = 2.f;
+static const float a1 = 8.f;
 static const float a2 = 0.8f;
-static const float b1 = 72.f;
-static const float b2 = 12.f;
+static const float b1 = 96.f;
+static const float b2 = 8.f;
 static const float c1 = 0.08125f;
 static const float c2 = 0.425f;
-static const float z1 = 0.4f;
-static const float z2 = 1.6f;
+static const float z1 = 0.42f;
+static const float z2 = 0.26f;
 
 static const uint8_t k_crossbands = 2;
 static const float bands[k_crossbands][4] = {
@@ -20,19 +20,24 @@ static const float bands[k_crossbands][4] = {
 
 /** /CONSTANTS */
 
-
-static inline float H(float x, float a, float b, float c, float z)
+/// @brief Tunable logistic function (sigmoid)
+/// @param a slope
+/// @param b slope 2
+/// @param c offset
+/// @param z portion scalar
+/// @return H(x)
+static inline float HP_H(float x, float a, float b, float c, float z)
 {
-    return z * a / (k_crossbands * a + fasterexpf(b * (c - x))) - 0.02f;
+    return z * a / (a + fasterexpf(b * (c - x))) - 0.02f;
 }
 
-static float resoH(float x)
+static float HP_resoH(float x)
 {
     float acc = 0;
 
     for (uint8_t band = 0; band < k_crossbands; band++)
     {
-        acc += H(x, bands[band][0], bands[band][1], bands[band][2], bands[band][3]);
+        acc += HP_H(x, bands[band][0], bands[band][1], bands[band][2], bands[band][3]);
     }
 
     return acc;
@@ -40,8 +45,8 @@ static float resoH(float x)
 
 void UserParameters::setHP(float p_value)
 {
-    p_hp_cutoff = fasterpowf(p_value, 0.5f) * 0.8;
-    p_hp_resonance = resoH(p_value) * 0.32f;
+    p_hp_cutoff = fasterpowf(p_value, 0.4f) * 0.8;
+    p_hp_resonance = HP_resoH(p_value) * 0.28f;
 }
 
 float UserParameters::getHPCutoff()
--- util.hpp
+++ util.hpp
@@ -1,3 +1,5 @@
+#include "osc_api.h"
+
 #pragma once
 
 // TB-303 style tanh approximation for feedback saturation
@@ -12,3 +14,14 @@ static inline float tb303_tanh(float x)
 #ifndef param_val_to_f32 
 #define param_val_to_f32(val) ((uint16_t)val * 9.77517106549365e-004f)
 #endif
+
+/// @brief Tunable logistic function (sigmoid)
+/// @param a slope
+/// @param b slope 2
+/// @param c offset
+/// @param z portion scalar
+/// @return H(x)
+static inline float H(float x, float a, float b, float c, float z)
+{
+    return z * a / (a + fasterexpf(b * (c - x))) - 0.02f;
+}
